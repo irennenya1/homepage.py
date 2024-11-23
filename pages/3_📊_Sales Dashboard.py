3de1dbd12b4e0d075509f3edf82e5baabea40122
irennenya1 <ryandika.official@gmail.com>
2024-11-23 23:17:12 +0700

    Update 3_📊_Sales Dashboard.py

--- pages/3_📊_Sales Dashboard.py
+++ pages/3_📊_Sales Dashboard.py
@@ -35,11 +35,6 @@ selected4 = option_menu(None, ["Home", "Menu", "Kontak", 'Settings'],
     icons=['house', 'cloud-upload', "list-task", 'gear'], 
     orientation="horizontal", manual_select=manual_select, key='menu_4')
 st.button(f"Next {st.session_state.get('menu_option', 1)}", key='switch_button')
-
-result = subprocess.run(['node', 'path/indeks.js'], capture_output=True, text=True)
-print(result.stdout) 
-
-
 # judul halaman 
 with st.container():
     st.markdown("<h1 class='centered-header'>Review Product </h1>", unsafe_allow_html=True)
